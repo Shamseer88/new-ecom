161c29f3002101484e22643204e2789f7acf0798
shamseer8877 <alishamseer@gmail.com>
2025-01-19 15:50:55 +0530

    Login and registration implemented

--- src/components/TopNavbar/TopNavbar.jsx
+++ src/components/TopNavbar/TopNavbar.jsx
@@ -1,12 +1,46 @@
+import React, { useEffect, useRef, useState } from "react";
 import "./TopNavbar.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { FaRegUser } from "react-icons/fa";
 import { FaRegHeart } from "react-icons/fa6";
 import { FiShoppingCart } from "react-icons/fi";
 import { useGender } from "../../context/GenderContext";
+import { useAuth } from "../../context/AuthContext";
 
 const TopNavbar = () => {
   const { setGender } = useGender();
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  const [showPopup, setShowPopup] = useState(false);
+  const popupRef = useRef(null);
+
+  const handleLogout = () => {
+    logout();
+    setShowPopup(false); // Close the popup after logout
+  };
+
+  const handleLoginRedirect = () => {
+    navigate("/login");
+    setShowPopup(false); // Close the popup after navigating to login
+  };
+
+  const handleRegisterRedirect = () => {
+    navigate("/register");
+    setShowPopup(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutsidePopup = (e) => {
+      if (popupRef.current && !popupRef.current.contains(e.target)) {
+        setShowPopup(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutsidePopup);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutsidePopup);
+    };
+  }, []);
+
   return (
     <div className="topnavbar">
       <div className="topnavbar-left">
@@ -31,7 +65,25 @@ const TopNavbar = () => {
         </NavLink>
       </div>
       <div className="topnavbar-right">
-        <FaRegUser className="user-icon" />
+        <div
+          className={user && "user-name"}
+          onClick={() => setShowPopup((prev) => !prev)} // Toggle popup visibility
+        >
+          <FaRegUser className="user-icon" />
+          {user && <p>Welcome {user.name}</p>}
+        </div>
+        {showPopup && (
+          <div className="popup" ref={popupRef}>
+            {user ? (
+              <button onClick={handleLogout}>Logout</button>
+            ) : (
+              <div className="popup-buttons-div">
+                <button onClick={handleLoginRedirect}>Login</button>
+                <button onClick={handleRegisterRedirect}>Register</button>
+              </div>
+            )}
+          </div>
+        )}
         <FaRegHeart size={17} className="heart-icon" />
         <FiShoppingCart size={18} className="cart-icon" />
       </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import axios from "axios";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { login } = useAuth();
   const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      const response = await axios.post(
+        "https://academics.newtonschool.co/api/v1/user/login",
+        {
+          email,
+          password,
+          appType: "ecommerce",
+        },
+        {
+          headers: {
+            projectID: "ulwb3huij7ys",
+          },
+        }
+      );
+      const { token } = response.data;
+      const { user } = response.data.data;
+      console.log("token", token);
+      console.log("user", user);
+
+      login(token, user);
+      navigate("/");
+    } catch (error) {
+      console.log("Login error", error);
+      setError("Invalid email or password!");
+    }
+  };
   return (
     <div className="register-div">
       <div className="register-form-div">
         <h2>Login</h2>
-        <form className="register-form">
+        <form className="register-form" onSubmit={handleSubmit}>
           <input
             type="email"
             name="email"
             placeholder="Email"
             required
             autoComplete="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
@@ -21,7 +60,10 @@ const Login = () => {
             placeholder="Password"
             required
             autoComplete="new-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
+          {error && <p style={{ color: "red" }}>{error}</p>}
           <button type="submit">Login</button>
         </form>
         <div className="already-have-an-account">
